acb688931180f98884dc7817f855f5c3cc5896b3
princeallan <allankirui57@gmail.com>
2023-06-28 01:12:32 +0300

    frontend components added

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,6 +3,7 @@ import {ref, onMounted, toRefs} from 'vue'
 import {Head, Link, router} from '@inertiajs/vue3';
 import MainLayout from '@/Layouts/MainLayout.vue';
 
+import ShowPostOverlay from '@/Components/ShowPostOverlay.vue'
 import LikesSection from '@/Components/LikesSection.vue'
 
 import 'vue3-carousel/dist/carousel.css'
@@ -82,6 +83,7 @@ onMounted(() => {
                     Post Text
                 </div>
                 <button
+                    @click="$event => openOverlay = true"
                     class="text-gray-500 font-extrabold py-1"
                 >
                     View all 10 comments
@@ -91,6 +93,12 @@ onMounted(() => {
             <div class="pb-20"></div>
         </div>
     </MainLayout>
+
+    <ShowPostOverlay
+        v-if="openOverlay"
+        :post="currentPost"
+        @closeOverlay="$event => openOverlay = false"
+    />
 
 </template>
 
